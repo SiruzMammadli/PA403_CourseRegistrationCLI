c0ff8da761d428a87cb239dd5b66f02a33e8bd12
sabinarasulova <sabina.rasulova2001@gmail.com>
2026-08-30 14:25:08 +0400

    Initial commit

---
+++ Domain/Data/GroupData.cs
@@ -0,0 +1,12 @@
+﻿using CourseRegistration.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseRegistration.Domain.Data
+{
+    public static class GroupData
+    {
+        private static List<Group> groups;
+    }
+}
---
+++ Domain/Models/Group.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseRegistration.Domain.Models
+{
+    internal class Group
+    {
+    }
+}
