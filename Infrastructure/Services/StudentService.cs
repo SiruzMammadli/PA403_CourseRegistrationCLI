8c77b0d66e3890da90df69ccc0898c868fae7f01
Siruz Mammadli <siruz.mammadli@gmail.com>
2026-08-30 14:00:32 +0400

    added student model & crudservice interface

---
+++ Infrastructure/Services/StudentService.cs
@@ -0,0 +1,16 @@
+﻿using CourseRegistration.Domain.Models;
+
+namespace CourseRegistration.Infrastructure.Services;
+
+public sealed class StudentService : ICrudService<Student>
+{
+    public List<Student> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Student GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
